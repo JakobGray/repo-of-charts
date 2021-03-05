cc64053b490fdbde70a53802c7543c4c35eb4776
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-03-05 18:17:00 -0500

    Use github api request to check latest sha before cloning repo

--- chart-sync.sh
+++ chart-sync.sh
@@ -1,23 +1,56 @@
 CHARTS_PATH="charts"
 CHART_VERSION="$(cat CHART_VERSION)"
+FORMAT=sha
 echo "Setting chart version as ${CHART_VERSION}"
-# rm charts/* #rm all charts first, in case chart versions are changed
+
+# temp-charts will hold the charts until it is ready to replace the current charts dir
 mkdir temp-charts
 
 while IFS=, read -r url chartpath shaorbranch
 do
-  printf "Github URL: $url\nPath to chart: $chartpath\nDesired sha: $shaorbranch\n"
 
-  latestsha=$(grep --max-count=1 "$url" latestSHAs.csv | cut -d ',' -f3)
+  if [[ $shaorbranch == master ]] || [[ $shaorbranch == release* ]];then
+    # This is a branch
+    printf "Github URL: $url\nPath to chart: $chartpath\nDesired branch: $shaorbranch\n"
+    FORMAT=branch
+  else
+    # Must be a sha
+    printf "Github URL: $url\nPath to chart: $chartpath\nDesired sha: $shaorbranch\n"
+    FORMAT=sha
+  fi
+
+  # currentsha is the sha of the chart currently bundled in the charts dir
+  currentsha=$(grep --max-count=1 "$url" currentSHAs.csv | cut -d ',' -f3)
+  # filename is the desired chart package name
   filename="${chartpath##*/}-${CHART_VERSION}.tgz"
 
   # Check if chart is using correct sha and chart version
-  if [ "$latestsha" == "$shaorbranch" ] && [ -f "charts/${filename}" ]; then
-    echo $"Latest SHA matches desired SHA and file exists. Copying to temp-charts."
+  if [ $FORMAT == sha ] && [ "$currentsha" == "$shaorbranch" ] && [ -f "charts/${filename}" ]; then
+    echo $"Current sha matches desired sha and chart file exists. Copying to chart over."
     cp "charts/${filename}" "temp-charts/${filename}"
-    echo -en "$url,$chartpath,$shaorbranch\n" >> temp-latestSHAs.csv
+    echo -en "$url,$chartpath,$shaorbranch\n" >> temp-currentSHAs.csv
     continue
   fi
+
+  if [ $FORMAT == branch ]; then
+    ## Check most recent sha in the repository branch
+
+    httpsTrimmedURL=${url#*//}
+    # githubTrimmedURL is the repo name without the leading 'https://github.com/'
+    githubTrimmedURL=${httpsTrimmedURL#*/}
+    echo $githubTrimmedURL
+
+    lastsha=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${githubTrimmedURL}/git/refs/heads/master | jq -r '.object.sha')
+    if [ "$currentsha" == "$lastsha" ] && [ -f "charts/${filename}" ]; then
+      # We already have this sha so no need to repackage
+      echo "Latest SHA matches SHA in branch ${shaorbranch} and file chart version exists."
+      cp "charts/${filename}" "temp-charts/${filename}"
+      echo -en "$url,$chartpath,$lastsha\n" >> temp-currentSHAs.csv
+      continue
+    fi
+  fi
+
+  echo "$url either does not have desired sha, doesn't have latest sha, or isn't set to the current chart version. It will need to be repackaged."
 
   # Work in temporary directory
   mkdir -p tmp
@@ -28,34 +61,36 @@ do
   cd */
   # Checkout branch or commit sha from origin
   git checkout $shaorbranch
-  newsha=$(git rev-parse HEAD)
-  echo $"New sha is ${newsha}"
+  lastsha=$(git rev-parse HEAD)
+  echo "Most recent sha is ${lastsha}"
 
-  if [ "$latestsha" == "$newsha" ]; then
-    echo "Latest SHA matches SHA in branch ${shaorbranch}"
-  fi
-  if [ -f "../../charts/${filename}" ]; then
-    echo "File exists in charts folder"
-  fi
+  # if [ "$currentsha" == "$lastsha" ]; then
+  #   echo "Latest SHA matches SHA in branch ${shaorbranch}"
+  # fi
+  # if [ -f "../../charts/${filename}" ]; then
+  #   echo "File exists in charts folder"
+  # fi
 
-  if [ "$latestsha" == "$newsha" ] && [ -f "../../charts/${filename}" ]; then
-    # We already have this sha so no need to repackage
-    echo "Latest SHA matches SHA in branch ${shaorbranch} and file chart version exists."
-    cp "../../charts/${filename}" "../../temp-charts/${filename}"
-  else
-    echo "Either shas don't match or the chart version has changed. Repackaging helm chart to temp-charts."
-    helm package $chartpath --version="${CHART_VERSION}" --destination="../../temp-charts"
-  fi
+  # if [ "$currentsha" == "$lastsha" ] && [ -f "../../charts/${filename}" ]; then
+  #   # We already have this sha so no need to repackage
+  #   echo "Latest SHA matches SHA in branch ${shaorbranch} and file chart version exists."
+  #   cp "../../charts/${filename}" "../../temp-charts/${filename}"
+  # else
+  #   echo "Either shas don't match or the chart version has changed. Repackaging helm chart to temp-charts."
+  #   helm package $chartpath --version="${CHART_VERSION}" --destination="../../temp-charts"
+  # fi
+
+  helm package $chartpath --version="${CHART_VERSION}" --destination="../../temp-charts"
 
   cd ../..
   rm -rf tmp
-  echo -en "$url,$chartpath,$newsha\n" >> temp-latestSHAs.csv
+  echo -en "$url,$chartpath,$lastsha\n" >> temp-currentSHAs.csv
 done < desiredSHAs.csv
 
 # cp "${CHARTS_PATH}/index.yaml" temp-charts/index.yaml
 rm -rf ${CHARTS_PATH}
 mv temp-charts ${CHARTS_PATH}
-mv temp-latestSHAs.csv latestSHAs.csv
+mv temp-currentSHAs.csv currentSHAs.csv
 git status --porcelain
 
 # if [[ `git status ${CHARTS_PATH} --porcelain` ]]; then
